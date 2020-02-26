ba8d36654a81d4ce5f9b420fc8682ff05dbd7704
ckapak <christinekapak@googlemail.com>
2020-02-26 16:30:15 +0000

    created Places model and serializer

---
+++ places/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Need to add another field? Add it in the relevant model and put default=''. makemigrations
+# Then select option 2 and migrate
+
+class Place(models.Model):
+  name = models.CharField(max_length=50) 
+  address = models.CharField(max_length=50)
+  postcode = models.CharField(max_length=8)
+  latitude = models.DecimalField(max_digits=6, decimal_places=3)
+  longitude = models.DecimalField(max_digits=6, decimal_places=3)
+  description = models.CharField(max_length=300, default='')
+  image = models.CharField(max_length=500)
+
+  def __str__(self):
+    return self.name
