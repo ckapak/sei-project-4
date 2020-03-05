f19745fdfa594829cc380af6bdf2b24dfbd1ca6c
ckapak <christinekapak@googlemail.com>
2020-03-05 18:14:05 +0000

    Add GDAL depends

--- project/settings.py
+++ project/settings.py
@@ -14,8 +14,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-GDAL_LIBRARY_PATH = "/app/.heroku/vendor/lib/libgdal.so"
-GEOS_LIBRARY_PATH = "/app/.heroku/vendor/lib/libgeos_c.so"
+GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -99,7 +99,6 @@ DATABASES = {
 import dj_database_url
 
 DATABASES['default'] =  dj_database_url.config()
-DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
